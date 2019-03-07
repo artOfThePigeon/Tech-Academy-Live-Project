e6affe35cef277a641787a2b6e57895fc7146f65
jordanspangenberg <jordan.spang@gmail.com>
2019-03-07 00:18:59 -0800

    update models to delete old avatar when creating new avatar

--- Forum/models.py
+++ Forum/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from TTA import settings
+import os
 
 
 def user_directory_path(instance, filename):
+  # TODO: Upload to media/user_<id>/avatar/<filename>
   #file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
   return 'avatar/user_{0}/{1}'.format(instance.User.id, filename)
 
@@ -15,28 +19,36 @@ class UserProfile(models.Model):
         """String for replacing the default 'UserProfile object 1' formatting """
         return f'{self.User.username} Profile'
 
-    # We need to see if the user is logged in before they update their profile.
     @classmethod
     def updateProfile(self, request, form):
-      # TODO: Delete old profile pics or reuse old pictures
+      # We need to see if the user is logged in before they update their profile.
       user = request.user
       if user.is_authenticated:
         userID = user.id
         sig = form['Signature']
+        #if the Avatar field is empty, value will be false, otherwise ImageFile
         avatar = request.FILES['Avatar'] if 'Avatar' in request.FILES else False
         try:
+          # I think we should create a UserProfile object on account registration.
           UserProfile.objects.create(User=User.objects.get(User_id=userID), Signature=sig, Avatar=avatar )
         except:
-          # Update the database
+          # Otherwise update the database
           profile = UserProfile.objects.get(User_id = userID)
           profile.Signature = sig
+          # if the Avatar field wasn't empty, do stuff
           if avatar != False:
+            avatar_url = profile.Avatar.url
+            # if the current avatar url DOES NOT match the default,
+            # delete the old avatar then create the new one
+            if not avatar_url.endswith('/media/avatar/default-avatar.png'):
+              if os.path.isfile(settings.BASE_DIR + avatar_url):
+                os.remove(settings.BASE_DIR + avatar_url)
             profile.Avatar = avatar
           profile.save()
 
       else:
-        # TODO: ask them to register or something
-        print("You're not logged in!")
+        #  ask them to register (or login?) if their user doesn't authenticate
+        HttpResponseRedirect('/register/')
 
 
 # Trying to upload the avatar file

ebd9e5a9ad98b9f308af3b2f0b97e68bbf8004c0
jordanspangenberg <jordan.spang@gmail.com>
2019-03-08 18:58:30 -0800

    Base funciton achieved, base template created

--- Forum/views.py
+++ Forum/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
 from django.template import RequestContext
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django import template
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from itertools import chain, groupby
@@ -20,80 +21,83 @@ from .models import UserProfile
 
 
 # Helper Fucntions
+
+# This makes a dictionary out of a custom SQL query
 def dictfetchall(query):
     "Return all rows from a cursor as a dict"
     with connection.cursor() as cursor:
-      cursor.execute(query)
-      columns = [col[0] for col in cursor.description]
-      return [
-          dict(zip(columns, row))
-          for row in cursor.fetchall()
-    ]
+        cursor.execute(query)
+        columns = [col[0] for col in cursor.description]
+        return [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
+
 
 
 
 # Account Views
 @login_required
 def home_view(request):
-    #collect data for latest 10 threads on homepage
-   data = Thread.objects.values().order_by('-DateUpdate')[:10]
-   #convert to dictionary to pass variable
-   threads = {"threads" : data}
-   return render(request, 'index.html', threads)
+    # collect data for latest 10 threads on homepage
+    data = Thread.objects.values().order_by('-DateUpdate')[:10]
+    # convert to dictionary to pass variable
+    threads = {"threads": data}
+    return render(request, 'index.html', threads)
 
 
 class UserProfileListView(generic.ListView):
-  template_name = 'user_profile/index.html'
-  context_object_name = 'users'
+    template_name = 'user_profile/index.html'
+    context_object_name = 'users'
 
 
 def get_profile(request):
-  # if this is a post request we need to process the form data
-  if request.method == 'POST':
-    # create a form instance and populate it with data from the request
-    form = ProfileForm(request.POST or None, request.FILES or None)
-    # check if valid
-    if form.is_valid():
-      # process the data in form as required
-      # redirect to a new URL
-      form = form.cleaned_data
-      UserProfile.updateProfile(request, form)
-      return HttpResponseRedirect('')
+    # if this is a post request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request
+        form = ProfileForm(request.POST or None, request.FILES or None)
+        # check if valid
+        if form.is_valid():
+            # process the data in form as required
+            # redirect to a new URL
+            form = form.cleaned_data
+            UserProfile.updateProfile(request, form)
+            return HttpResponseRedirect('')
+        else:
+            print(form.errors)
+
     else:
-      print(form.errors)
+        form = ProfileForm()
+    context = {
+        'form': form,
+    }
 
-  else:
-    form = ProfileForm()
-  context = {
-    'form' : form,
-  }
-
-  return render(request, 'user_profile/index.html', context)
+    return render(request, 'user_profile/index.html', context)
 
 
 def register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-          user = form.save()
-          user.refresh_from_db()
-          UserProfile.objects.create(User_id=user.pk)
-          raw_password = form.cleaned_data.get('password1')
-          user = authenticate(username=user.username, password=raw_password)
-          login(request, user)
-          return redirect('home_view')
+            user = form.save()
+            user.refresh_from_db()
+            UserProfile.objects.create(User_id=user.pk)
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home_view')
     else:
-      form = SignUpForm()
+        form = SignUpForm()
     return render(request, 'Accounts/register.html', {'form': form})
 
 
-# Forum views
+# Return a dictionary of thread topics
 class TopicsView(generic.ListView):
-  model = Topic
-  template_name = 'topics.html'
+    model = Topic
+    template_name = 'topics.html'
 
-  def get_queryset(self):
-    return dictfetchall('''SELECT Forum_thread.id as thread_id,
+    def get_queryset(self):
+        return dictfetchall('''SELECT Forum_thread.id as thread_id,
                             Forum_topic.TopicTitle as "topic_title",
                             ThreadTitle as thread_title,
                             Forum_topic.ThreadCount as thread_count,
@@ -103,6 +107,44 @@ class TopicsView(generic.ListView):
                             ON Forum_topic.id = Forum_thread.Topic_id
                             GROUP BY Topic_id
                             ORDER BY Forum_topic.id ASC''')
+
+
+# Display the comments of a thread
+class CommentThread(generic.ListView):
+    template_name = 'forum/comment_thread.html'
+    context_object_name = 'comments'
+    paginated_by = 10
+    ordering = ['-DateCreated']
+
+    def get_queryset(self, *args, **kwargs):
+        pk = self.kwargs['pk']
+        return Comment.objects.filter(Thread_id=pk).order_by('DateCreated').reverse
+
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(CommentThread, self).get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+        context['thread'] = Thread.objects.filter(id = pk).last
+        #context['user'] = User.objects.all()
+
+        return context
+    # This gets the username from the user_id
+
+    register = template.Library()
+
+    @register.simple_tag
+    def get_username_from_userid(user_id):
+      '''
+        Which you would use like:
+
+        {% for comment in comments %}
+          {% get_username_from_userid comment.user_id %}
+        {% endfor %}
+      '''
+      try:
+          return User.objects.get(id=user_id).username
+      except User.DoesNotExist:
+          return 'Unknown'
 
 # Messaging
 def message(request):
@@ -115,33 +157,34 @@ def message(request):
         content = request.POST['content']
         reciever = request.POST['reciever']
 
+        # Gets the username of the receiver from the db
+        reciever_used = User.objects.get(username=reciever)
+        sender_used = user  # Sender is current user
 
-        reciever_used = User.objects.get(username=reciever)# Gets the username of the receiver from the db
-        sender_used = user # Sender is current user
-
-        message = Message() # Creating connection to the db
+        message = Message()  # Creating connection to the db
         # Setting respective table fields to template content
         message.MessageBody = content
         message.ReceivingUser = reciever_used
         message.SendingUser = sender_used
-        message.save() # Saving to the db
+        message.save()  # Saving to the db
 
         # Return information to send message
-        return render(request, 'Message/message.html', {'message': message} )
+        return render(request, 'Message/message.html', {'message': message})
 
     else:
 
         # Query the FriendConnection table to get the ids of current users friends
-        friend_list = FriendConnection.objects.filter((Q(ReceivingUser=user) | Q(SendingUser=user))& Q(IsConfirmed=True))
+        friend_list = FriendConnection.objects.filter(
+            (Q(ReceivingUser=user) | Q(SendingUser=user)) & Q(IsConfirmed=True))
         # Create a list of friends user ids
         friend_ids = list(friend_list.values('ReceivingUser_id'))
-        receiver_id_list = [] # Empty list for the ids
+        receiver_id_list = []  # Empty list for the ids
 
         # Iterate through the list, appending the user ids
         for id in friend_ids:
             receiver_id_list.append(id['ReceivingUser_id'])
 
-        receiver_name_list = [] # Empty list for the receiver username
+        receiver_name_list = []  # Empty list for the receiver username
 
         # Iterate through the id list, grabbing the username and appending it to the new empty list
         for id in receiver_id_list:
@@ -155,15 +198,18 @@ def message(request):
         }
 
         # Return list of friends usernames
-        return render(request,'Message/message.html', context)
+        return render(request, 'Message/message.html', context)
 
 # Main Inbox
+
+
 def inbox(request):
 
     # Populate all messages to current user.
     if request.user.is_authenticated:
         currentUser = request.user.id
-        messages = Message.objects.filter(ReceivingUser_id=currentUser).order_by('-DateSent')
+        messages = Message.objects.filter(
+            ReceivingUser_id=currentUser).order_by('-DateSent')
 
         # Return messages to be placed in template
         return render(request, 'Message/inbox.html', {'messages': messages})
@@ -172,16 +218,20 @@ def inbox(request):
         return render(request, 'Message/inbox.html', {})
 
 # Message Details (Shows all messages from selected sender)
+
+
 def messagedetails(request):
 
-
-    currentUser = request.user.id # Set current user
-    message_id = request.GET.get('message_id') # Get the id of the selected message from Main Inbox
-    message = Message.objects.get(id=message_id) # Get the message entry from the db
-    sendingUser = message.SendingUser.id # Get the sending user id from the db
+    currentUser = request.user.id  # Set current user
+    # Get the id of the selected message from Main Inbox
+    message_id = request.GET.get('message_id')
+    # Get the message entry from the db
+    message = Message.objects.get(id=message_id)
+    sendingUser = message.SendingUser.id  # Get the sending user id from the db
 
     # Query the db for all messages from selected sender and order decending
-    messages = Message.objects.filter(ReceivingUser_id=currentUser, SendingUser_id=sendingUser).order_by('-DateSent')
+    messages = Message.objects.filter(
+        ReceivingUser_id=currentUser, SendingUser_id=sendingUser).order_by('-DateSent')
 
     # Return the list of messages to the template to be placed
     return render(request, 'Message/messagedetails.html', {'messages': messages})
